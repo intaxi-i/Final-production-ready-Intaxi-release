7586907c8cbc7110d506915bb029d2467f82d37a
Hanif <jurabekjkj@gmail.com>
2026-04-30 05:06:45 +0500

    Wire backend v2 public routes

--- backend_v2/app/main.py
+++ backend_v2/app/main.py
@@ -8,6 +8,7 @@ from app.api.v2.routes_admin import router as admin_router
 from app.api.v2.routes_city import router as city_router
 from app.api.v2.routes_driver import router as driver_router
 from app.api.v2.routes_intercity import router as intercity_router
+from app.api.v2.routes_public import router as public_router
 from app.core.config import get_settings
 from app.core.errors import DomainError
 
@@ -41,3 +42,4 @@ app.include_router(city_router, prefix=settings.api_prefix)
 app.include_router(driver_router, prefix=settings.api_prefix)
 app.include_router(intercity_router, prefix=settings.api_prefix)
 app.include_router(admin_router, prefix=settings.api_prefix)
+app.include_router(public_router, prefix=settings.api_prefix)
