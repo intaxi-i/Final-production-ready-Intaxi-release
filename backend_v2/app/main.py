670152ab606d30a8b28d62ea3e7b108b2fff2002
Hanif <jurabekjkj@gmail.com>
2026-05-01 00:32:48 +0500

    Register backend v2 support routes

--- backend_v2/app/main.py
+++ backend_v2/app/main.py
@@ -9,6 +9,7 @@ from app.api.v2.routes_city import router as city_router
 from app.api.v2.routes_driver import router as driver_router
 from app.api.v2.routes_intercity import router as intercity_router
 from app.api.v2.routes_public import router as public_router
+from app.api.v2.routes_support import router as support_router
 from app.api.v2.routes_user import router as user_router
 from app.api.v2.routes_wallet import router as balance_router
 from app.core.config import get_settings
@@ -45,5 +46,6 @@ app.include_router(city_router, prefix=settings.api_prefix)
 app.include_router(driver_router, prefix=settings.api_prefix)
 app.include_router(balance_router, prefix=settings.api_prefix)
 app.include_router(intercity_router, prefix=settings.api_prefix)
+app.include_router(support_router, prefix=settings.api_prefix)
 app.include_router(admin_router, prefix=settings.api_prefix)
 app.include_router(public_router, prefix=settings.api_prefix)
