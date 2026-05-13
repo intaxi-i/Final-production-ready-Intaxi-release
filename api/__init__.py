a366acf41f07dfba0616328141230d432a57b5ce
Hanif <jurabekjkj@gmail.com>
2026-05-13 15:00:10 +0500

    Install production patch from api package init

--- api/__init__.py
+++ api/__init__.py
@@ -1,5 +1,9 @@
 """FastAPI backend package for InTaxi mini app."""
 
+from api.intaxi_production_patch import install_intaxi_production_patch
+
+install_intaxi_production_patch()
+
 __all__ = ["__version__"]
 
 __version__ = "0.1.0"
