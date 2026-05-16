89391751c346b28ff0e07dd9e55160302d659018
Hanif <jurabekjkj@gmail.com>
2026-05-16 21:05:35 +0500

    Install strict API city accept patch

--- api/__init__.py
+++ api/__init__.py
@@ -1,12 +1,14 @@
 """FastAPI backend package for InTaxi mini app."""
 
 from api.country_bootstrap import apply_country_config
+from api.intaxi_accept_patch import install_intaxi_accept_patch
 from api.intaxi_production_patch import install_intaxi_production_patch
 from api.intaxi_safety_patch import install_intaxi_safety_patch
 
 apply_country_config()
 install_intaxi_production_patch()
 install_intaxi_safety_patch()
+install_intaxi_accept_patch()
 
 __all__ = ["__version__"]
 
