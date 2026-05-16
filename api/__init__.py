246c6fe122fae75c58b7a1bb30aee98f106ea481
Hanif <jurabekjkj@gmail.com>
2026-05-16 17:54:40 +0500

    Apply country config during API bootstrap

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,9 @@
 """FastAPI backend package for InTaxi mini app."""
 
+from api.country_bootstrap import apply_country_config
 from api.intaxi_production_patch import install_intaxi_production_patch
 
+apply_country_config()
 install_intaxi_production_patch()
 
 __all__ = ["__version__"]
