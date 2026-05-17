f8b7d1c3ea75429439c020802c0e309eeee95874
Hanif <jurabekjkj@gmail.com>
2026-05-17 18:27:07 +0500

    Install router route patch before API registration

--- api/__init__.py
+++ api/__init__.py
@@ -5,10 +5,12 @@ from api.intaxi_accept_patch import install_intaxi_accept_patch
 from api.intaxi_intercity_patch import install_intaxi_intercity_patch
 from api.intaxi_intercity_status_patch import install_intaxi_intercity_status_patch
 from api.intaxi_production_patch import install_intaxi_production_patch
+from api.intaxi_router_patch import install_router_route_patch
 from api.intaxi_safety_patch import install_intaxi_safety_patch
 from api.intaxi_terminal_patch import install_intaxi_terminal_patch
 
 apply_country_config()
+install_router_route_patch()
 install_intaxi_production_patch()
 install_intaxi_safety_patch()
 install_intaxi_accept_patch()
