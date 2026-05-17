1c849e61adde5ea72d288c22de36d56e76bcb9da
Hanif <jurabekjkj@gmail.com>
2026-05-17 11:02:37 +0500

    Install strict API intercity status patch

--- api/__init__.py
+++ api/__init__.py
@@ -3,6 +3,7 @@
 from api.country_bootstrap import apply_country_config
 from api.intaxi_accept_patch import install_intaxi_accept_patch
 from api.intaxi_intercity_patch import install_intaxi_intercity_patch
+from api.intaxi_intercity_status_patch import install_intaxi_intercity_status_patch
 from api.intaxi_production_patch import install_intaxi_production_patch
 from api.intaxi_safety_patch import install_intaxi_safety_patch
 
@@ -11,6 +12,7 @@ install_intaxi_production_patch()
 install_intaxi_safety_patch()
 install_intaxi_accept_patch()
 install_intaxi_intercity_patch()
+install_intaxi_intercity_status_patch()
 
 __all__ = ["__version__"]
 
